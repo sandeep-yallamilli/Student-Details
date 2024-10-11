b93a909ca16ab8c25b828f8072509099eadfd96e
Sandeep Yallamilli <110769926+sandeep-yallamilli@users.noreply.github.com>
2024-10-11 09:03:33 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('students', views.index,name='index'),
     path('insert',views.insertdata,name='insert'),
     path('update/<id>',views.updateData,name='update'),
     path('delete/<id>',views.deleteData,name='delete')
